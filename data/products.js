c9d573a0eab0f3960ec7fedc712230e79ee9634a
John Hunter <johnroberthunter1@gmail.com>
2026-02-11 09:06:51 -0700

    got the products data and page added and styled

---
+++ data/products.js
@@ -0,0 +1,39 @@
+const products = [
+    {
+        id: 1,
+        name: 'Basketball',
+        price: '$19.99',
+        description: 'Official size and weight basketball for indoor and outdoor play.',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7a/Basketball.png/250px-Basketball.png'
+    },
+    {
+        id: 2,
+        name: 'Running Shoes',
+        price: '$100',
+        description: 'High-performance running shoes for all-day comfort.',
+        image: 'https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/aefedd69-868a-4bd2-a484-b383dcae7a67/custom-nike-air-zoom-pegasus-41-shoes-by-you.png'
+    },
+    {
+        id: 3,
+        name: 'Fitness Tracker',
+        price: '$49.99',
+        description: 'Track your daily steps and heart rate with this sleek fitness tracker.',
+        image: 'https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1685983818-61gFrSlmofL.jpg?crop=0.726xw:0.726xh;0.146xw,0.128xh&resize=980:*'
+    },
+    {
+        id: 4,
+        name: 'Yoga Mat',
+        price: '$29.99',
+        description: 'Non-slip yoga mat for all-day comfort.',
+        image: 'https://i5.walmartimages.com/seo/Wakeman-Fitness-1-2-In-Extra-Thick-Yoga-Mat-With-Carrying-Strap-Black_0d2f1fb8-1450-4fff-904d-d1bb49618dda_1.d03df211e3a6ca16eae6ffc9cf16a77a.jpeg'
+    },
+    {
+        id: 5,
+        name: 'Headphones',
+        price: '$79.99',
+        description: 'High-quality headphones for immersive audio experience.',
+        image: 'https://www.kroger.com/product/images/xlarge/front/0081006114507'
+    },
+]
+
+export default products;
